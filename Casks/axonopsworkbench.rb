d6fc92a89f2ff99606ce1d72b7c4b83ab52fd6d9
Sergio Rua <sergio.rua@digitalis.io>
2024-07-31 12:59:24 +0100

    Syntax checks

--- Casks/axonopsworkbench.rb
+++ Casks/axonopsworkbench.rb
@@ -1,10 +1,11 @@
 cask "axonopsworkbench" do
   name "AxonOps Workbench"
   desc "AxonOps Workbench"
-  homepage "https://axonops.com"
+  homepage "https://github.com/axonops/axonops-workbench-cassandra"
   version "0.1.0-internal-20240729"
-
   arch arm: "arm64", intel: "x86_64"
+  sha256 arm:   "03d3dfaa20e9ec24561803e45211ac7bbefe9f839b0f45b40ce52ac8c0326b0e",
+         intel: "b622c4279829f6e4f812c5a1aa9991dd12beeb55887aafecd934951bbee74f97"
 
   url "https://github.com/axonops/axonops-workbench-cassandra/releases/download/#{version}/AxonOpsWorkbench-#{version}-mac-#{arch}.zip"
 
