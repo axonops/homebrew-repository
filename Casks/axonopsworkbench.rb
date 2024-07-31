2a9426af3d98c209c2df6e482290b12bb7e8d885
Sergio Rua <sergio.rua@digitalis.io>
2024-07-31 13:06:11 +0100

    Syntax checks

--- Casks/axonopsworkbench.rb
+++ Casks/axonopsworkbench.rb
@@ -1,24 +1,25 @@
 cask "axonopsworkbench" do
-  name "AxonOps Workbench"
-  desc "AxonOps Workbench"
-  homepage "https://github.com/axonops/axonops-workbench-cassandra"
+  arch arm: "arm64", intel: "amd64"
+
   version "0.1.0-internal-20240729"
-  arch arm: "arm64", intel: "x86_64"
   sha256 arm:   "03d3dfaa20e9ec24561803e45211ac7bbefe9f839b0f45b40ce52ac8c0326b0e",
          intel: "b622c4279829f6e4f812c5a1aa9991dd12beeb55887aafecd934951bbee74f97"
 
   url "https://github.com/axonops/axonops-workbench-cassandra/releases/download/#{version}/AxonOpsWorkbench-#{version}-mac-#{arch}.zip"
+  name "AxonOps Workbench"
+  desc "This Cask install the AxonOps Workbench application"
+  homepage "https://axonops.com/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  app "AxonOpsWorkbench.app"
   depends_on macos: ">= :big_sur"
-  zap trash: [
-    "~/Library/Application Scripts/AxonOpsWorkbench"
-  ]
+
+  app "AxonOpsWorkbench.app"
+
+  zap trash: "~/Library/Application Scripts/AxonOpsWorkbench"
 end
 
 # code: language=ruby
