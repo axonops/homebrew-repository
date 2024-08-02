2cbaa8de7372c9f09c05da7af28a095fe4d51bdc
Sergio Rua <sergio.rua@digitalis.io>
2024-08-02 09:58:18 +0100

    Fixes syntax

--- Casks/axonopsworkbench.rb
+++ Casks/axonopsworkbench.rb
@@ -1,33 +1,8 @@
 cask "axonopsworkbench" do
   arch arm: "arm64", intel: "x64"
 
-  version "%VERSION%"
+  version "0.1.0-internal-20240802"
   sha256 arm:   "6f7204c204f823343e5c5eeb572d7716db7324aebcdd347e6dd6d3cf932462e6",
-         intel: "%SHA_AMD%"
-
-  url "https://github.com/axonops/axonops-workbench-cassandra/releases/download/#{version}/AxonOps.Workbench-#{version}-mac-#{arch}.zip"
-  name "AxonOps Workbench"
-  desc "This Cask install the AxonOps Workbench application"
-  homepage "https://github.com/axonops/axonops-workbench-cassandra/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
-  depends_on macos: ">= :big_sur"
-
-  app "AxonOps Workbench.app"
-
-  zap trash: "~/Library/Application Scripts/AxonOps Workbench"
-end
-
-# code: language=ruby
-cask "axonopsworkbench" do
-  arch arm: "arm64", intel: "x64"
-
-  version "%VERSION%"
-  sha256 arm:   "%SHA_ARM%",
          intel: "06d5b8ec9bbaa93f91663692e9794d7758fdbcb1b8c35df9e0e647581e8bbe35"
 
   url "https://github.com/axonops/axonops-workbench-cassandra/releases/download/#{version}/AxonOps.Workbench-#{version}-mac-#{arch}.zip"
@@ -46,30 +21,3 @@ cask "axonopsworkbench" do
 
   zap trash: "~/Library/Application Scripts/AxonOps Workbench"
 end
-
-# code: language=ruby
-cask "axonopsworkbench" do
-  arch arm: "arm64", intel: "x64"
-
-  version "0.1.0-internal-20240802"
-  sha256 arm:   "%SHA_ARM%",
-         intel: "%SHA_AMD%"
-
-  url "https://github.com/axonops/axonops-workbench-cassandra/releases/download/#{version}/AxonOps.Workbench-#{version}-mac-#{arch}.zip"
-  name "AxonOps Workbench"
-  desc "This Cask install the AxonOps Workbench application"
-  homepage "https://github.com/axonops/axonops-workbench-cassandra/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
-  depends_on macos: ">= :big_sur"
-
-  app "AxonOps Workbench.app"
-
-  zap trash: "~/Library/Application Scripts/AxonOps Workbench"
-end
-
-# code: language=ruby
