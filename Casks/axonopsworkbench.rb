a5d3a9dc95d858582aad48ec1faea80acc08ec5c
Sergio Rua <sergio.rua@digitalis.io>
2024-10-29 15:03:42 +0000

    Fixes version

--- Casks/axonopsworkbench.rb
+++ Casks/axonopsworkbench.rb
@@ -5,7 +5,7 @@ cask "axonopsworkbench" do
   sha256 arm:   "aaf95585415ba43868b91efdda290000c6ea2585d6d8f3c5d826240f19baa277",
          intel: "0df5c266dfefc112d0ea8bf6cccfa1ec2800ca8cd1184384d1e7895fd44dba0b"
 
-  url "https://github.com/axonops/axonops-workbench/releases/download/#{version}/AxonOps.Workbench-#{version}-mac-#{arch}.zip"
+  url "https://github.com/axonops/axonops-workbench/releases/download/#{version}/AxonOps.Workbench-#{version.sub('v', '')}-mac-#{arch}.zip"
   name "AxonOps Workbench"
   desc "This Cask install the AxonOps Workbench application"
   homepage "https://github.com/axonops/axonops-workbench/"
