1090c21f4ecc655c41ce5a9a8d027d3303632bc5
Sergio Rua <sergio@axonops.com>
2025-10-21 17:39:56 +0200

    Fix syntax

---
+++ kconduit.rb
@@ -0,0 +1,24 @@
+class Kconduit < Formula
+  desc "kconduit is a fast, portable, and AI-enhanced interactive terminal for Apache Kafka, built in Go"
+  homepage "https://github.com/digitalis-io/kconduit"
+  url "https://github.com/digitalis-io/kconduit/archive/refs/tags/v0.0.14.tar.gz"
+          sha256 "aa414d6ec6f055518904a3892cec72bb4d2c60f162035a59f760229c7a72505b"
+  license "Apache-2.0"
+  head "https://github.com/digitalis-io/kconduit.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64: "84f95370f9ed2cb7846e1b9dee5d5feb8a5991b3515ec77505d77726461329b0"
+    sha256 cellar: :any_skip_relocation, amd64: "caaf08b5a2ef0fa197110f9dbebca872f2167b06a85c2c63b0407be5121ecdd5"
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.Version=v0.0.14"), "cmd/kconduit/main.go"
+  end
+
+  test do
+    status_output = shell_output("#{bin}/kconduit --version 2>&1", 1)
+    assert_match "Cannot run the application", status_output
+  end
+end
